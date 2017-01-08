880b1a74d2c2e6bd360672903883ee5e73be03df
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-01-08 11:22:10 +0200

    Way to add and remove users and workers from hub

--- gate/hub.go
+++ gate/hub.go
@@ -19,6 +19,58 @@ func NewHub() *Hub {
 	}
 }
 
+func (h *Hub) AddUser(user *User) error {
+	h.usersLock.Lock()
+	defer h.usersLock.Unlock()
+
+	if _, ok := h.users[user.ID()]; ok {
+		return fmt.Errorf("users map already has a record with id of %d", user.ID())
+	}
+
+	h.users[user.ID()] = user
+
+	return nil
+}
+
+func (h *Hub) RemoveUser(user *User) error {
+	h.usersLock.Lock()
+	defer h.usersLock.Unlock()
+
+	if _, ok := h.users[user.ID()]; !ok {
+		return fmt.Errorf("user not found with id of %d", user.ID())
+	}
+
+	delete(h.users, user.ID())
+
+	return nil
+}
+
+func (h *Hub) AddWorker(worker *Worker) error {
+	h.workersLock.Lock()
+	defer h.workersLock.Unlock()
+
+	if _, ok := h.workers[worker.ID()]; ok {
+		return fmt.Errorf("workers map already has a record with id of %d", worker.ID())
+	}
+
+	h.workers[worker.ID()] = worker
+
+	return nil
+}
+
+func (h *Hub) RemoveWorker(worker *Worker) error {
+	h.workersLock.Lock()
+	defer h.workersLock.Unlock()
+
+	if _, ok := h.workers[worker.ID()]; !ok {
+		return fmt.Errorf("worker not found with id of %d", worker.ID())
+	}
+
+	delete(h.workers, worker.ID())
+
+	return nil
+}
+
 func (h *Hub) UserByID(id int) (*User, error) {
 	h.usersLock.RLock()
 	defer h.usersLock.RUnlock()
