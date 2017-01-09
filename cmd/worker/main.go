3b110824efdff2c175c4df9601dbc3b07c3dafdd
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-01-09 15:50:45 +0200

    Worker as basic socket client for gate

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jvikstedt/bluemoon/bluemoon"
+	"github.com/jvikstedt/bluemoon/socket"
+)
 
 func main() {
-	fmt.Println("worker")
+	sClient := socket.NewClient()
+	conn, err := sClient.Connect("gate:5000")
+	if err != nil {
+		panic(err)
+	}
+
+	cw := socket.NewConnectionWrapper(conn)
+	defer cw.Close()
+
+	w := bluemoon.NewBaseClient(1, cw, func(client bluemoon.Client, data []byte) {
+		fmt.Printf("New message from gate: %d\n", client.ID())
+		fmt.Print(string(data))
+	})
+
+	go w.EnableReader()
+	w.EnableWriter()
 }
