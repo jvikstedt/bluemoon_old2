af082bab89acb3edada8748f5c9d90f745fe5d31
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-01-17 08:23:29 +0200

    Return error from change_dir PlayerEntity cast fails

--- worker/event/change_dir.go
+++ worker/event/change_dir.go
@@ -1,6 +1,7 @@
 package event
 
 import (
+	"fmt"
 	"github.com/jvikstedt/bluemoon/worker/entity"
 	"github.com/jvikstedt/bluemoon/worker/room"
 )
@@ -14,12 +15,14 @@ type ChangeDir struct {
 func (cd *ChangeDir) Execute(room *room.Room) error {
 	e := room.EntityById(cd.ID)
 	playerEntity, ok := e.(*entity.PlayerEntity)
-	if ok {
-		if cd.Axis == "x" {
-			playerEntity.SetXDir(cd.Val)
-		} else if cd.Axis == "y" {
-			playerEntity.SetYDir(cd.Val)
-		}
+	if !ok {
+		return fmt.Errorf("Wrong type of entity, expected PlayerEntity with id of: %d", cd.ID)
+	}
+
+	if cd.Axis == "x" {
+		playerEntity.SetXDir(cd.Val)
+	} else if cd.Axis == "y" {
+		playerEntity.SetYDir(cd.Val)
 	}
 	return nil
 }
