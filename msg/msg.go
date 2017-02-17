11ace80c86360e5c8d511d8ac705297aa8ca83b3
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-02-17 16:31:06 +0200

    Fixed packer Pack method

--- msg/msg.go
+++ msg/msg.go
@@ -19,12 +19,10 @@ func (p Packer) Unpack(data []byte) (name string, payload []byte, err error) {
 		name = string(data[:id])
 		payload = data[id+1:]
 	}
-
-	return name, payload, nil
+	return
 }
 
 func (p Packer) Pack(name string, payload []byte) (data []byte, err error) {
-	name = name + ";"
-
-	return append([]byte(name), payload...), nil
+	data = append(append([]byte(name), p.separator), payload...)
+	return
 }
